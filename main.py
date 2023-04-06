422e375e8c5744d735c763a406fff401b3c81b48
kd2929 <110597375+kd2929@users.noreply.github.com>
2023-04-06 15:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [
+
   
 
 keyboard = InlineKeyboardMarkup(
